e7e0c2d4d284cbe2727c73b8c96597b7a8747955
Franklin Marden <127708250+FranklinMSR@users.noreply.github.com>
2024-06-02 09:05:11 -0300

    Created and initializing controllers

---
+++ src/Action.API/Controllers/AuctionController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Action.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuctionController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetCurrentAuction() 
+    {
+        return Ok("Franklin");
+    }
+}
